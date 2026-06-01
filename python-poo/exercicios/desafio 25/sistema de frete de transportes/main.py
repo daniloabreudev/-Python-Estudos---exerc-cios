435cbef4489e82b12daf8c255915a6a15fdbd4c1
Danilo Abreu <abreudanilojdas456@gmail.com>
2026-06-01 09:19:39 -0300

    Cria um sitema de cálculo fretes com poo

--- python-poo/exercicios/desafio 25/sistema de frete de transportes/main.py
+++ python-poo/exercicios/desafio 25/sistema de frete de transportes/main.py
@@ -18,6 +18,18 @@ class Moto(Transporte):
     def calc_frete(self):
         pass
 
+def main():
+    dist= 80
+    viagem = [Moto(dist),Caminhao(dist), Drone(dist)]
+    # entrega = Drone(dist)
+    # print(f"Frete de {type(entrega).__name__} em {dist}km = {entrega.calc_frete()}")
+    tabela = Table(title="Tabela de Fretes")
+    tabela.add_column("Distância")
+    tabela.add_column("Tipo")
+    tabela.add_column("Frete")
+    for item in viagem:
+        tabela.add_row(f"{dist}km",f"{type(item).__name__}",f"{item.calc_frete()}")
+    print(tabela)
 
 class Caminhao(Transporte):
     def __init__(self, taxa=0):
