6e19d0a7f07c089b1ab41c89127c9850a2a187e1
Danilo Abreu <abreudanilojdas456@gmail.com>
2026-06-04 09:14:19 -0300

    Calcula salario de funcionários

--- python-poo/exercicios/desafio 26/funcionarios.py
+++ python-poo/exercicios/desafio 26/funcionarios.py
@@ -3,40 +3,42 @@ from rich.panel import Panel
 from rich import print
 
 class Funcionario(ABC):
-    def __init__(self,nome,salario):
+    salario_min = 1612
+    desconto_inss = 7.5
+    def __init__(self,nome = None):
         self.nome = nome
         self.sal_bruto = 0
-        self.salario = salario
-        self.sal_min = 1612
-        self.inss = 7.5
+        self.salario = 0
 
     @abstractmethod
     def calc_sal(self):
         pass
 
     def analisar_sal(self):
-        conteudo = f" O salário de [blue]{self.nome}[/] é de [green]{self.calc_sal()}[/]\n e corresponde a [yellow]{self.calc_sal()/ self.sal_min:.2f}[/] salários mínimos."
+        base = self.salario / Funcionario.salario_min
+        conteudo = f" O salário de [blue]{self.nome}[/] (Funcionário {self.__class__.__name__}) é de [green]{self.salario:.2f}[/]\n e corresponde a [yellow]{base:.1f}[/] salários mínimos."
         print(Panel(conteudo,title="Análise de sálario",expand=False))
 
 class Horista(Funcionario):
-    def __init__(self,nome,valor_hora,horas_trab):
-        super().__init__(nome,valor_hora)
+    def __init__(self,nome,valor_hora = 7.37,horas_trab = 220):
+        super().__init__(nome)
         self.valor_hora = valor_hora
         self.horas_trab = horas_trab
+        self.sal_bruto = self.valor_hora *self.horas_trab
 
     def calc_sal(self):
-        self.sal_bruto = self.valor_hora * self.horas_trab
-        desconto = (self.inss * self.sal_bruto)/100
-        return self.sal_bruto - desconto
+        self.salario = self.sal_bruto - (self.sal_bruto * Funcionario.desconto_inss/100)
+
 
 
 class Mensalista(Funcionario):
-    def __init__ (self,nome, salario):
-        super().__init__(nome,salario)
+    def __init__ (self,nome, salario = 1612):
+        super().__init__(nome)
+        self.sal_bruto = salario
 
     def calc_sal(self):
-        desconto = (self.inss * self.salario)/100
-        return self.salario - desconto
+        self.salario = self.sal_bruto - (self.sal_bruto * Funcionario.desconto_inss/100)
+
 
 
 
