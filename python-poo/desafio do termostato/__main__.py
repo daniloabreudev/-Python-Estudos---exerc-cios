3c65f3671998ca1e3fb1041b6e665b8a22eb6c78
Danilo Abreu <abreudanilojdas456@gmail.com>
2026-06-17 09:14:09 -0300

    correção

--- python-poo/desafio do termostato/__main__.py
+++ python-poo/desafio do termostato/__main__.py
@@ -2,7 +2,7 @@ from termostato import Termostato
 
 def main():
     t1 = Termostato()
-    t1.temperatura = 29.5
+    t1.temperatura = 29.8
     print(f"TEMPERATURA ATUAL: {t1.temperatura} graus celsius")
 
 if __name__== "__main__":
--- python-poo/desafio do termostato/termostato.py
+++ python-poo/desafio do termostato/termostato.py
@@ -13,4 +13,4 @@ class Termostato:
         if 16<= graus <=30 and (graus % 0.5 == 0):
             self.__temperatura = graus
         else:
-            print(f"O [green] termostato[/] só aceita valores em [blue]16°[/] e [blue]30°[/] graus,de meio em meio grau! EX: 18.5,valores como 18.4 não são aceitos")
+            print(f"O [green] termostato[/] só aceita valores em [blue]16°[/] e [blue]30°[/] graus,de meio em meio grau! EX: 18.5. valores como 18.4 não são aceitos")
