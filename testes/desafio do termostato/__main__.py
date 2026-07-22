4ab606cba840937ab00bbcc6f9b1520b744a8845
Danilo Abreu <abreudanilojdas456@gmail.com>
2026-07-22 07:24:47 -0300

    Update __main__.py

--- testes/desafio do termostato/__main__.py
+++ testes/desafio do termostato/__main__.py
@@ -3,7 +3,7 @@ from termostato import Termostato
 def main():
     t = Termostato()
     t.temperatura = 32
-    t.temperatura = 23.59
+    t.temperatura = 23.50
     print(f"A temperatura atual é {t.temperatura}")
     print(t.ftemperatura)
 
